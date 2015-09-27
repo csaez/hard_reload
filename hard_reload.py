c88abeb74820b9493fb5e170094b894d9a8619c4
csaez <cesarte@gmail.com>
2015-09-27 18:09:22 +1000

    Decouple GUI

--- hard_reload.py
+++ hard_reload.py
@@ -7,45 +7,52 @@ class HardReload(QtGui.QDialog):
 
     def __init__(self, parent=None):
         super(HardReload, self).__init__(parent)
-        self.initUI()
+        self.setWindowTitle("Hard Reload")
 
-    def initUI(self):
         self.ui_lineEdit = QtGui.QLineEdit(self)
         ui_completer = QtGui.QCompleter(
             [i.split(".")[0] for i in sys.modules.keys()], self)
         ui_completer.setCompletionMode(QtGui.QCompleter.InlineCompletion)
         ui_completer.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
         self.ui_lineEdit.setCompleter(ui_completer)
+
         hbox = QtGui.QHBoxLayout()
         hbox.addWidget(self.ui_lineEdit)
         self.setLayout(hbox)
-        self.setWindowTitle("Hard Reload")
+
         self.ui_lineEdit.returnPressed.connect(self.accept)
 
+    def move_window(self, pos=None):
+        pos = pos or QtGui.QCursor.pos()
+        self.move(pos.x(), pos.y())
+
     def accept(self, *args, **kwds):
-        module = str(self.ui_lineEdit.text())
-        if len(module):
-            for k in sys.modules.keys():
-                if k.startswith(module):
-                    del sys.modules[k]
+        module_name = str(self.ui_lineEdit.text())
+        if len(module_name):
+            hard_reload(module_name)
         super(HardReload, self).close(*args, **kwds)
 
 
-def show():
+def get_parent():
     parent = QtGui.QApplication.activeWindow()
     if parent:
         _ = parent.parent()
         while _:
             parent = _
             _ = parent.parent()
-    w = HardReload(parent)
-    position_window(w)
+    return parent
+
+
+def hard_reload(module_name):
+    for k in sys.modules.keys():
+        if k.startswith(module_name):
+            del sys.modules[k]
+
+
+def show():
+    w = HardReload(get_parent())
+    w.move_window()
     w.exec_()
-
-
-def position_window(window):
-    pos = QtGui.QCursor.pos()
-    window.move(pos.x(), pos.y())
 
 
 def __main__():
